e42e2250d92ad225d1cb8604d791e442096b0b08
Mike Eldredge <maeldredge.pro@gmail.com>
2023-03-17 08:37:57 -0600

    courses.js: organize/alphabetize code & functions

--- scripts/courses.js
+++ scripts/courses.js
@@ -1,23 +1,24 @@
-/////////////
-// Globals //
-/////////////
+//------------------//
+// Global Constants //
+//------------------//
 const MAX_HOLES = 18;
 const CLS_HEADER = 'header';
 const CLS_CTRL_ROW = 'control-row';
 const CLS_COURSE_INFO = 'course-info';
 const CLS_SEL_COURSE_INFO = `.${CLS_COURSE_INFO}`;
-
 const COURSE_LIST_URL =
     'https://maeldredgepro.github.io/LetsPlayGolf/data/courses.json';
 
-function courseDataURL(courseID) {
+// This function is in the Constants section because is more like a macro.
+function COURSE_DATA_URL(courseID) {
     return 'https://maeldredgepro.github.io/LetsPlayGolf/data/course'
         + courseID
         + '.txt';
 }
 
-window.onload = fetchCourseList;
-
+//------------------------------//
+// Global Data type definitions //
+//------------------------------//
 class TeeBoxInfo {
     static datumLabels = [
         'Yards',
@@ -43,9 +44,91 @@ class TeeBoxInfo {
     }
 }
 
-//////////////////////////
-// Function Definitions //
-//////////////////////////
+//------------------//
+// Global Variables //
+//------------------//
+var g_playModeActive = false;
+const g_playerList = [];
+
+//-----------------------//
+// Global Excutable Code //
+//-----------------------//
+
+// Code run when the script is loaded.  The only thing it does is register
+// the (main-ish) function to be called once the html has finished loading.
+window.onload = fetchCourseList;
+
+
+//----------------------------//
+// Globally Visible Functions //
+//----------------------------//
+
+function appendAddPlayerButton(courseInfoTable) {
+    // Create a row and cell for the user to click on to add a player.
+    const addPlayerButton = newElement('button');
+    addPlayerButton.innerText = 'Add a player';
+    addPlayerButton.addEventListener('click', handleClickAddPlayerButton);
+    const addPlayerCell = newElement('td');
+    addPlayerCell.appendChild(addPlayerButton);
+    const addPlayerRow = newElement('tr', CLS_CTRL_ROW);
+    addPlayerRow.appendChild(addPlayerCell);
+    courseInfoTable.appendChild(addPlayerRow);
+}
+
+function appendControlButtons(courseInfoTable) {
+    if (g_playerList.length < 4) {
+        appendAddPlayerButton(courseInfoTable);
+    }
+
+    if (g_playerList.length > 0) {
+        appendPlayButton(courseInfoTable);
+    }
+}
+
+function appendPlayButton(courseInfoTable) {
+    // Create a row and cell for the user to click on to add a player.
+    const playButton = newElement('button');
+    playButton.innerText = 'Play Golf!';
+    playButton.addEventListener('click', handleClickPlayButton);
+    const playButtonCell = newElement('td');
+    playButtonCell.appendChild(playButton);
+    const playButtonRow = newElement('tr', CLS_CTRL_ROW);
+    playButtonRow.appendChild(playButtonCell);
+    courseInfoTable.appendChild(playButtonRow);
+}
+
+function createCourseInfoTable() {
+    return newElement('table', CLS_COURSE_INFO);
+}
+
+function createCourseInfoTableHeaderRow(numHoles) {
+    const tableHeaderRow = newElement('tr', CLS_HEADER);
+
+    // Add the label 'Tee' to the table header row.
+    let tableRowLabel = newElement('th');
+    tableHeaderRow.appendChild(tableRowLabel);
+    tableRowLabel.innerText = 'Tee';
+
+    // Add the label 'Hole' to the table header row.
+    tableRowLabel = newElement('th');
+    tableRowLabel.innerText = 'Hole';
+    tableHeaderRow.appendChild(tableRowLabel);
+
+    for (let holeNum = 1; holeNum <= numHoles; ++holeNum) {
+        const tableHeaderData = newElement('th');
+        tableHeaderData.innerText = holeNum;
+        tableHeaderRow.appendChild(tableHeaderData);
+    }
+
+    return tableHeaderRow;
+}
+
+function fetchCourseInfo(courseID) {
+    fetch(courseDataURL(courseID))
+        .then(response => response.json())
+        .then(courseData => renderCourseData(courseData.data.holes));
+}
+
 function fetchCourseList() {
     // launch a promise to fetch the course list.  When complete, it will
     // call back a function to render the course selection page.
@@ -54,43 +137,67 @@ function fetchCourseList() {
         .then(courseList => renderCourseSelectionPage(courseList));
 }
 
-function renderCourseSelectionPage(courses) {
-    const courseSelect = newElement('select');
+function generateInfoGrid(holesData) {
+    const infoGrid = [];
 
-    // Add a 'prompt' option which can't be selected,
-    // but will prompt the user to make a course selection.
-    const promptOption = newElement('option');
-    promptOption.selected = true;
-    promptOption.disabled = true;
-    promptOption.innerText = 'Select a course:';
-    courseSelect.appendChild(promptOption);
+    holesData.forEach((holeData, holeIndex) => {
+        // Iterate the tee box data for this hole.  Each tee box constitutes
+        // a row group, since there are multiple data points we are interested
+        // in per tee box.
+        holeData.teeBoxes.forEach((teeBoxData, teeBoxIndex) => {
+            const teeBoxInfo = new TeeBoxInfo(teeBoxData);
+            // Iterate through the data points in the tee box we are interested in
+            TeeBoxInfo.datumLabels.forEach((datumLabel, datumSelector) => {
+                const colNumber = holeIndex;
 
-    // Add the available course options.
-    courses.forEach(course => {
-        const courseOption = newElement('option');
-        courseOption.setAttribute('id', course.id);
-        courseOption.innerText = course.name;
-        courseSelect.appendChild(courseOption);
+                // Add another column if we need to
+                if (infoGrid.length <= colNumber) {
+                    infoGrid.push([]);
+                }
+
+                // create the datum
+                const tableDatum = teeBoxInfo.GetDatum(datumSelector);
+                infoGrid[colNumber].push(tableDatum);
+            })
+        })
     })
 
-    // Finished creating the course select dropdown.
-    // Add its event listener and add it to the page.
-    courseSelect.addEventListener('change', handleChangeCourseSelect);
-    document.body.appendChild(courseSelect);
+    return infoGrid;
 }
 
-function handleChangeCourseSelect(event) {
-    const courseSelect = event.target;
-    const courseID = courseSelect[courseSelect.selectedIndex].id;
-    playerList.length = 0;
-    fetchCourseInfo(courseID);
-    // The fetch registers a callback that will render the course info.
+function installCourseInfoTable(newCourseInfoTable) {
+    // remove the old course info table
+    const oldCourseInfoTable = document.querySelector(CLS_SEL_COURSE_INFO);
+    if (oldCourseInfoTable) {
+        oldCourseInfoTable.remove();
+    }
+
+    if (newCourseInfoTable) {
+        document.body.appendChild(newCourseInfoTable);
+    }
 }
 
-function fetchCourseInfo(courseID) {
-    fetch(courseDataURL(courseID))
-        .then(response => response.json())
-        .then(courseData => renderCourseData(courseData.data.holes));
+// from https://stackoverflow.com/questions/175739/how-can-i-check-if-a-string-is-a-valid-number
+function isNumeric(str) {
+    if (typeof str != "string") return false // we only process strings!
+    return !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
+        !isNaN(parseFloat(str)) // ...and ensure strings of whitespace fail
+}
+
+function newElement(tag, classAttrValue = null) {
+    const element = document.createElement(tag);
+
+    if (classAttrValue) {
+        element.setAttribute('class', classAttrValue);
+    }
+
+    return element;
+}
+
+function removeControlButtons(courseInfoTable) {
+    while (courseInfoTable.lastChild.className === CLS_CTRL_ROW) {
+        courseInfoTable.lastChild.remove();
+    }
 }
 
 function renderCourseData(holesData) {
@@ -162,126 +269,44 @@ function renderCourseData(holesData) {
     installCourseInfoTable(courseInfoTable);
 }
 
-function generateInfoGrid(holesData) {
-    const infoGrid = [];
+function renderCourseSelectionPage(courses) {
+    const courseSelect = newElement('select');
 
-    holesData.forEach((holeData, holeIndex) => {
-        // Iterate the tee box data for this hole.  Each tee box constitutes
-        // a row group, since there are multiple data points we are interested
-        // in per tee box.
-        holeData.teeBoxes.forEach((teeBoxData, teeBoxIndex) => {
-            const teeBoxInfo = new TeeBoxInfo(teeBoxData);
-            // Iterate through the data points in the tee box we are interested in
-            TeeBoxInfo.datumLabels.forEach((datumLabel, datumSelector) => {
-                const colNumber = holeIndex;
+    // Add a 'prompt' option which can't be selected,
+    // but will prompt the user to make a course selection.
+    const promptOption = newElement('option');
+    promptOption.selected = true;
+    promptOption.disabled = true;
+    promptOption.innerText = 'Select a course:';
+    courseSelect.appendChild(promptOption);
 
-                // Add another column if we need to
-                if (infoGrid.length <= colNumber) {
-                    infoGrid.push([]);
-                }
-
-                // create the datum
-                const tableDatum = teeBoxInfo.GetDatum(datumSelector);
-                infoGrid[colNumber].push(tableDatum);
-            })
-        })
+    // Add the available course options.
+    courses.forEach(course => {
+        const courseOption = newElement('option');
+        courseOption.setAttribute('id', course.id);
+        courseOption.innerText = course.name;
+        courseSelect.appendChild(courseOption);
     })
 
-    return infoGrid;
+    // Finished creating the course select dropdown.
+    // Add its event listener and add it to the page.
+    courseSelect.addEventListener('change', handleChangeCourseSelect);
+    document.body.appendChild(courseSelect);
 }
 
-function createCourseInfoTable() {
-    return newElement('table', CLS_COURSE_INFO);
+function handleChangeCourseSelect(event) {
+    const courseSelect = event.target;
+    const courseID = courseSelect[courseSelect.selectedIndex].id;
+    g_playerList.length = 0;
+    fetchCourseInfo(courseID);
+    // The fetch registers a callback that will render the course info.
 }
 
-function createCourseInfoTableHeaderRow(numHoles) {
-    const tableHeaderRow = newElement('tr', CLS_HEADER);
-
-    // Add the label 'Tee' to the table header row.
-    let tableRowLabel = newElement('th');
-    tableHeaderRow.appendChild(tableRowLabel);
-    tableRowLabel.innerText = 'Tee';
-
-    // Add the label 'Hole' to the table header row.
-    tableRowLabel = newElement('th');
-    tableRowLabel.innerText = 'Hole';
-    tableHeaderRow.appendChild(tableRowLabel);
-
-    for (let holeNum = 1; holeNum <= numHoles; ++holeNum) {
-        const tableHeaderData = newElement('th');
-        tableHeaderData.innerText = holeNum;
-        tableHeaderRow.appendChild(tableHeaderData);
-    }
-
-    return tableHeaderRow;
-}
-
-function installCourseInfoTable(newCourseInfoTable) {
-    // remove the old course info table
-    const oldCourseInfoTable = document.querySelector(CLS_SEL_COURSE_INFO);
-    if (oldCourseInfoTable) {
-        oldCourseInfoTable.remove();
-    }
-
-    if (newCourseInfoTable) {
-        document.body.appendChild(newCourseInfoTable);
-    }
-}
-
-function handleClickTeeTypeButton(event) {
-    const selectedTeeType = event.target.innerText;
-    console.log(`'${selectedTeeType}' button clicked`);
-
-    // Get the course info table.
-    const courseInfoTable = document.querySelector(CLS_SEL_COURSE_INFO);
-
-    // Hide all of the rows except the ones belonging to the selected tee type.
-    let curClassName = '';
-    for (row of courseInfoTable.rows) {
-        if (row.className != 'undefined') {
-            curClassName = row.className
-        }
-
-        // Leave the header row and the selected tee rows alone.
-        if (curClassName === CLS_HEADER) { continue; }
-        if (curClassName === selectedTeeType) { continue; }
-
-        // Hide the rows we don't care about.
-        row.style.display = 'none';
-    }
-
-    // Add the control buttons.
-    appendControlButtons(courseInfoTable);
-}
-
-function appendControlButtons(courseInfoTable) {
-    if (playerList.length < 4) {
-        appendAddPlayerButton(courseInfoTable);
-    }
-
-    if (playerList.length > 0) {
-        appendPlayButton(courseInfoTable);
-    }
-}
-
-function appendAddPlayerButton(courseInfoTable) {
-    // Create a row and cell for the user to click on to add a player.
-    const addPlayerButton = newElement('button');
-    addPlayerButton.innerText = 'Add a player';
-    addPlayerButton.addEventListener('click', handleClickAddPlayerButton);
-    const addPlayerCell = newElement('td');
-    addPlayerCell.appendChild(addPlayerButton);
-    const addPlayerRow = newElement('tr', CLS_CTRL_ROW);
-    addPlayerRow.appendChild(addPlayerCell);
-    courseInfoTable.appendChild(addPlayerRow);
-}
-
-const playerList = [];
 function handleClickAddPlayerButton(event) {
     const newPlayer = prompt('Add a player');
 
     // Reject the player if they have already been added.
-    playerList.forEach(player => {
+    g_playerList.forEach(player => {
         if (newPlayer.toLowerCase() === player.toLowerCase()) {
             alert(`${player} has already been added`);
             return;
@@ -289,7 +314,7 @@ function handleClickAddPlayerButton(event) {
     })
 
     // Add the player to our internal player list.
-    playerList.push(newPlayer);
+    g_playerList.push(newPlayer);
 
     // Get the course info table.
     const courseInfoTable = document.querySelector(CLS_SEL_COURSE_INFO);
@@ -319,34 +344,8 @@ function handleClickAddPlayerButton(event) {
     appendControlButtons(courseInfoTable);
 }
 
-function appendPlayButton(courseInfoTable) {
-    // Create a row and cell for the user to click on to add a player.
-    const playButton = newElement('button');
-    playButton.innerText = 'Play Golf!';
-    playButton.addEventListener('click', handleClickPlayButton);
-    const playButtonCell = newElement('td');
-    playButtonCell.appendChild(playButton);
-    const playButtonRow = newElement('tr', CLS_CTRL_ROW);
-    playButtonRow.appendChild(playButtonCell);
-    courseInfoTable.appendChild(playButtonRow);
-}
-
-function handleClickPlayButton() {
-    const courseInfoTable = document.querySelector(CLS_SEL_COURSE_INFO);
-    removeControlButtons(courseInfoTable);
-    alert("Let's Play Golf!");
-    playing = true;
-}
-
-function removeControlButtons(courseInfoTable) {
-    while (courseInfoTable.lastChild.className === CLS_CTRL_ROW) {
-        courseInfoTable.lastChild.remove();
-    }
-}
-
-let playing = false;
 function handleClickAddStrokes(event) {
-    if (!playing) { return; }
+    if (!g_playModeActive) { return; }
 
     const strokes = prompt('How many strokes?');
 
@@ -372,19 +371,35 @@ function handleClickAddStrokes(event) {
     console.log(event.target);
 }
 
-// from https://stackoverflow.com/questions/175739/how-can-i-check-if-a-string-is-a-valid-number
-function isNumeric(str) {
-    if (typeof str != "string") return false // we only process strings!
-    return !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
-        !isNaN(parseFloat(str)) // ...and ensure strings of whitespace fail
+function handleClickPlayButton() {
+    const courseInfoTable = document.querySelector(CLS_SEL_COURSE_INFO);
+    removeControlButtons(courseInfoTable);
+    alert("Let's Play Golf!");
+    g_playModeActive = true;
 }
 
-function newElement(tag, classAttrValue = null) {
-    const element = document.createElement(tag);
+function handleClickTeeTypeButton(event) {
+    const selectedTeeType = event.target.innerText;
+    console.log(`'${selectedTeeType}' button clicked`);
 
-    if (classAttrValue) {
-        element.setAttribute('class', classAttrValue);
+    // Get the course info table.
+    const courseInfoTable = document.querySelector(CLS_SEL_COURSE_INFO);
+
+    // Hide all of the rows except the ones belonging to the selected tee type.
+    let curClassName = '';
+    for (row of courseInfoTable.rows) {
+        if (row.className != 'undefined') {
+            curClassName = row.className
+        }
+
+        // Leave the header row and the selected tee rows alone.
+        if (curClassName === CLS_HEADER) { continue; }
+        if (curClassName === selectedTeeType) { continue; }
+
+        // Hide the rows we don't care about.
+        row.style.display = 'none';
     }
 
-    return element;
+    // Add the control buttons.
+    appendControlButtons(courseInfoTable);
 }
